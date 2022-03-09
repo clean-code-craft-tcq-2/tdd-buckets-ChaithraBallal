080af9c18c5a55373c99825c9112fbf1e251ce81
ChaithraBallal <91113671+ChaithraBallal@users.noreply.github.com>
2022-03-09 20:49:18 +0530

    Update Test.cpp

--- Test.cpp
+++ Test.cpp
@@ -5,7 +5,7 @@
 TEST_CASE("Infers the output range for the given set of inputs")
 {
   int InputCurrentRange[] = {2,3,3,4,5};
-  char *ComputedRange;
+  char ComputedRange;
   int result;
   int NumberofCurrentSamples = sizeof(InputCurrentRange)/sizeof(InputCurrentRange[0]);
   ComputedRange = CalculateCurrentRanges(InputCurrentRange,NumberofCurrentSamples);
