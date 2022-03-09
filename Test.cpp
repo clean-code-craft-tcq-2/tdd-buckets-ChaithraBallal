25dd835a5e9b217e9b9f8daae22c69a922b527c0
ChaithraBallal <91113671+ChaithraBallal@users.noreply.github.com>
2022-03-09 22:04:18 +0530

    Update Test.cpp

--- Test.cpp
+++ Test.cpp
@@ -16,3 +16,19 @@ TEST_CASE("Infers the output range for the given set of inputs")
   result = strcmp(ComputedRange, "2-5,5");
   REQUIRE(result == 0);
 }
+
+TEST_CASE("Find the lower out of the array")
+{
+  int InputCurrentRange[] = {15,6,7,8,9};
+  int NoofValues = (sizeof(InputCurrentRange))/(sizeof(InputCurrentRange[0]));
+  int LowValue = findLowerLimit(InputCurrentRange,NoofValues);
+  REQUIRE(LowValue == 6);
+}
+
+TEST_CASE("Find the lower out of the array")
+{
+  int InputCurrentRange[] = {15,26,19,99,9};
+  int NoofValues = (sizeof(InputCurrentRange))/(sizeof(InputCurrentRange[0]));
+  int HighValue = findUpperLimit(InputCurrentRange,NoofValues);
+  REQUIRE(HighValue == 99);
+}
