f51db141ae50b6efe978b28b98a3f64bd5c0bd34
ChaithraBallal <91113671+ChaithraBallal@users.noreply.github.com>
2022-03-09 20:46:00 +0530

    Update MonitoringCharge.h

--- MonitoringCharge.h
+++ MonitoringCharge.h
@@ -1,4 +1,6 @@
 #include <stdio.h>
 
 
-extern char* CalculateCurrentRanges(InputCurrentRange,NumberofCurrentSamples);
+extern char* CalculateCurrentRanges( int* InputCurrentRange, int NumberofCurrentSamples);
+extern int findUpperLimit(int* currentRange, int NoofCurrentValues);
+extern int findLowerLimit(int* currentRange, int NoofCurrentValues);
