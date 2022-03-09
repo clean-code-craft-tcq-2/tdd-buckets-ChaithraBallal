5fdea40499658184d4622cafce054aee2aeb388b
ChaithraBallal <91113671+ChaithraBallal@users.noreply.github.com>
2022-03-09 20:45:57 +0530

    Update MonitoringCharge.c

--- MonitoringCharge.c
+++ MonitoringCharge.c
@@ -1,1 +1,38 @@
+#include <stdio.h>
+#include "string.h"
+#include "MonitoringCharge.h"
 
+int findLowerLimit(int* currentRange, int NoofCurrentValues)
+{
+  int lowerLimit = currentRange[0];
+  for(i=1; i<NoofCurrentValues; i++)
+  {
+    if (lowerlimit < currentRange[i])
+    {
+      lowerlimit = currentRange[i];
+    }
+  }
+  return lowerlimit;
+}
+
+int findUpperLimit(int* currentRange, int NoofCurrentValues)
+{
+  int upperLimit = currentRange[0];
+  for(i=1; i<NoofCurrentValues; i++)
+  {
+    if (upperLimit > currentRange[i])
+    {
+       upperLimit = currentRange[i];
+    }
+  }
+  return upperLimit;
+}
+
+char* CalculateCurrentRanges( int* InputCurrentRange, int NumberofCurrentSamples)
+{
+  char outputBuffer;
+  int lowCurrentValue = findLowerLimit(InputCurrentRange,NumberofCurrentSamples);
+  int highCurrentValue = findUpperLimit(InputCurrentRange,NumberofCurrentSamples);
+  sprintf(outputBuffer, "%d-%d,%d",lowCurrentValue,highCurrentValue,NumberofCurrentSamples);
+  return outputBuffer;  
+}
