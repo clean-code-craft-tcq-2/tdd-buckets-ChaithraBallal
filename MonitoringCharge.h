a5b4434f77ed3e415ed10bb4798da2d388c3530c
ChaithraBallal <91113671+ChaithraBallal@users.noreply.github.com>
2022-04-01 20:14:18 +0530

    Update MonitoringCharge.h

--- MonitoringCharge.h
+++ MonitoringCharge.h
@@ -6,4 +6,4 @@
 extern char* CalculateCurrentRanges( int InputCurrentRange[], int NumberofCurrentSamples);
 extern int findUpperLimit(int currentRange[], int NoofCurrentValues);
 extern int findLowerLimit(int currentRange[], int NoofCurrentValues);
-extern int* funcA2DConverter(int InputAnalogCurrentValue[]), int sizeofInput);
+extern int* funcA2DConverter(int InputAnalogCurrentValue[], int sizeofInput);
