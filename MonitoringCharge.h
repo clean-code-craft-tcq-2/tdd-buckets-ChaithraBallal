d0c561bdd31bfe79cc602fdb6ca383fcf837c57a
ChaithraBallal <91113671+ChaithraBallal@users.noreply.github.com>
2022-04-01 22:40:03 +0530

    Update MonitoringCharge.h

--- MonitoringCharge.h
+++ MonitoringCharge.h
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 #define MaxcurrentValue      4094
-
+#define MAX_CURRENT_AMPS     10
 extern char* CalculateCurrentRanges( int InputCurrentRange[], int NumberofCurrentSamples);
 extern int findUpperLimit(int currentRange[], int NoofCurrentValues);
 extern int findLowerLimit(int currentRange[], int NoofCurrentValues);
