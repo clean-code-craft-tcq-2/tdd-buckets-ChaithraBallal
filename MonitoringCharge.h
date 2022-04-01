67dd9ac7e634b6c54ce9dbc1673e7b9aa05b2121
ChaithraBallal <91113671+ChaithraBallal@users.noreply.github.com>
2022-04-01 20:11:49 +0530

    Update MonitoringCharge.h

--- MonitoringCharge.h
+++ MonitoringCharge.h
@@ -1,5 +1,9 @@
 #include <stdio.h>
 
+#define MaxcurrentValue      4094u
+#define MincurrentValue      0u
+#define InvalidCurrentValue  4095u /*Assumption made for an invalid current value and mentioned in the md file as well*/
 extern char* CalculateCurrentRanges( int InputCurrentRange[], int NumberofCurrentSamples);
 extern int findUpperLimit(int currentRange[], int NoofCurrentValues);
 extern int findLowerLimit(int currentRange[], int NoofCurrentValues);
+extern int* funcA2DConverter(int InputAnalogCurrentValue[]),int sizeofInput);
