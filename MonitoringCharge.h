f7e7aaad4877120f081466a354dc9155f4b35041
ChaithraBallal <91113671+ChaithraBallal@users.noreply.github.com>
2022-04-01 22:28:50 +0530

    Update MonitoringCharge.h

--- MonitoringCharge.h
+++ MonitoringCharge.h
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 #define MaxcurrentValue      4094
-#define InvalidCurrentValue  0 /*Assumption made for an invalid current value and mentioned in the md file as well*/
+
 extern char* CalculateCurrentRanges( int InputCurrentRange[], int NumberofCurrentSamples);
 extern int findUpperLimit(int currentRange[], int NoofCurrentValues);
 extern int findLowerLimit(int currentRange[], int NoofCurrentValues);
